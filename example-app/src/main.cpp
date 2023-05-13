2769eaa5a7e1f98d3a1d3c0a7cd796084ca75708
erbsland-dev <github@erbsland.dev>
2023-05-13 12:25:16 +0200

    Extend the example to make it more visual.
    - Adding an example TOML file that is parsed.
    - Copy the parsed configuration to the binary so the example will work out of the box.
    - Adding a display function so there is output in the console.

--- example-app/src/main.cpp
+++ example-app/src/main.cpp
@@ -7,18 +7,40 @@
 #include <iostream>
 
 
-using elqt::toml::Parser;
-using elqt::toml::Error;
+using namespace elqt::toml;
+
+
+void displayValue(const ValuePtr &value, const QString &path) {
+    std::cout << path.toStdString() << "\n";
+    if (value->isTable()) {
+        std::cout << "Table: " << value->size() << " Elements" << "\n\n";
+        for (const auto &[key, tableValue] : value->toTable()) {
+            displayValue(tableValue, path + '/' + key);
+        }
+    } else if (value->isArray()) {
+        std::cout << "Array: " << value->size() << " Elements" << "\n\n";
+        auto array = value->toArray();
+        for (std::size_t i = 0; i < array.size(); ++i) {
+            displayValue(array.at(i), path + QStringLiteral("/[%1]").arg(i));
+        }
+    } else {
+        std::cout << "Value: " << value->toVariant().toString().toStdString() << "\n\n";
+    }
+}
 
 
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
+    QCoreApplication application(argc, argv);
+    ValuePtr toml{};
     try {
+        auto path = QCoreApplication::applicationDirPath() + "/config.toml";
         Parser parser{};
-        auto toml = parser.parseFile("config.toml");
-        // ...
+        std::cout << "Reading configuration from: " << path.toStdString() << "\n";
+        toml = parser.parseFile(path);
     } catch (const Error &err) {
         std::cerr << err.toString().toStdString() << std::endl;
-        exit(1);
+        return 1;
     }
+    displayValue(toml, "[root]");
+    return 0;
 }
